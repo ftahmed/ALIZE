bb7c8511f52181b8f110f613fac32748cbbf276c
antho <antho@05c9c20c-f3ed-481d-8948-15215b5a29c9>
2010-04-21 13:13:42 +0000

    add cstdio in include

--- src/FeatureComputer.cpp
+++ src/FeatureComputer.cpp
@@ -64,6 +64,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <memory>
 #include <cmath>
 #include <iostream>
+#include <cstdio>
 //#include "spro.h"
 
 #define OM_EPSILON 0.00001
